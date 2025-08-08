f22ba65c5f12da66ebac0c4cad45266fdc6d4d37
Rohit Shidid <rrs6770@nyu.edu>
2025-08-08 16:18:33 +0000

    Updates in tail.c with more functionality

--- user/tail.c
+++ user/tail.c
@@ -8,12 +8,14 @@
 char buf[MAX_LINES][MAX_LINE];
 
 void print_help() {
-  printf("Usage: tail [-n N] [file]\n");
-  printf("Print the last N lines of a file or from standard input.\n");
+  printf("Usage: tail [-n N | -N | +N] [file]\n");
+  printf("Print the last N lines of a file, or start from line +N.\n");
   printf("Examples:\n");
   printf("  tail file.txt           Show last 10 lines of file.txt\n");
   printf("  tail -n 5 file.txt      Show last 5 lines\n");
-  printf("  tail -n 0 file.txt      Show nothing\n");
+  printf("  tail -5 file.txt        Shorthand for last 5 lines\n");
+  printf("  tail +3 file.txt        Show from line 3 to end\n");
+  printf("  tail -n +3 file.txt     Same as above\n");
   printf("  tail                    Read from stdin\n");
 }
 
@@ -27,6 +29,7 @@ int is_number(const char *s) {
 
 int main(int argc, char *argv[]) {
   int fd = 0, n = 10, total = 0;
+  int start_from_line = -1; // -1 means last N lines mode, else start from this line
   char line[MAX_LINE];
   int line_len = 0, c;
 
@@ -35,34 +38,86 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
 
+  // Argument parsing
   if (argc == 1) {
     fd = 0;
-  } else if (argc == 2) {
-    fd = open(argv[1], 0);
-    if (fd < 0) {
-      fprintf(2, "tail: cannot open %s\n", argv[1]);
+  }
+  else if (argc == 2) {
+    // tail <filename> OR tail -N OR tail +N (stdin)
+    if (argv[1][0] == '-' && is_number(argv[1] + 1)) {
+      n = atoi(argv[1] + 1);
+      fd = 0;
+    } else if (argv[1][0] == '+' && is_number(argv[1] + 1)) {
+      start_from_line = atoi(argv[1] + 1);
+      fd = 0;
+    } else {
+      fd = open(argv[1], 0);
+      if (fd < 0) {
+        fprintf(2, "tail: cannot open %s\n", argv[1]);
+        exit(1);
+      }
+    }
+  }
+  else if (argc == 3) {
+    if (strcmp(argv[1], "-n") == 0) {
+      if (argv[2][0] == '+') {
+        if (!is_number(argv[2] + 1)) {
+          fprintf(2, "tail: invalid number: %s\n", argv[2]);
+          exit(1);
+        }
+        start_from_line = atoi(argv[2] + 1);
+      } else {
+        if (!is_number(argv[2])) {
+          fprintf(2, "tail: invalid number: %s\n", argv[2]);
+          exit(1);
+        }
+        n = atoi(argv[2]);
+      }
+      fd = 0;
+    }
+    else if (argv[1][0] == '-' && is_number(argv[1] + 1)) {
+      n = atoi(argv[1] + 1);
+      fd = open(argv[2], 0);
+      if (fd < 0) {
+        fprintf(2, "tail: cannot open %s\n", argv[2]);
+        exit(1);
+      }
+    }
+    else if (argv[1][0] == '+' && is_number(argv[1] + 1)) {
+      start_from_line = atoi(argv[1] + 1);
+      fd = open(argv[2], 0);
+      if (fd < 0) {
+        fprintf(2, "tail: cannot open %s\n", argv[2]);
+        exit(1);
+      }
+    }
+    else {
+      fprintf(2, "Usage: tail [-n N | -N | +N] [file]\n");
       exit(1);
     }
-  } else if (argc == 3 && strcmp(argv[1], "-n") == 0) {
-    if (!is_number(argv[2])) {
-      fprintf(2, "tail: invalid number of lines: %s\n", argv[2]);
-      exit(1);
+  }
+  else if (argc == 4 && strcmp(argv[1], "-n") == 0) {
+    if (argv[2][0] == '+') {
+      if (!is_number(argv[2] + 1)) {
+        fprintf(2, "tail: invalid number: %s\n", argv[2]);
+        exit(1);
+      }
+      start_from_line = atoi(argv[2] + 1);
+    } else {
+      if (!is_number(argv[2])) {
+        fprintf(2, "tail: invalid number: %s\n", argv[2]);
+        exit(1);
+      }
+      n = atoi(argv[2]);
     }
-    n = atoi(argv[2]);
-    fd = 0;
-  } else if (argc == 4 && strcmp(argv[1], "-n") == 0) {
-    if (!is_number(argv[2])) {
-      fprintf(2, "tail: invalid number of lines: %s\n", argv[2]);
-      exit(1);
-    }
-    n = atoi(argv[2]);
     fd = open(argv[3], 0);
     if (fd < 0) {
       fprintf(2, "tail: cannot open %s\n", argv[3]);
       exit(1);
     }
-  } else {
-    fprintf(2, "Usage: tail [-n N] [file]\n");
+  }
+  else {
+    fprintf(2, "Usage: tail [-n N | -N | +N] [file]\n");
     exit(1);
   }
 
@@ -70,7 +125,9 @@ int main(int argc, char *argv[]) {
     fprintf(2, "tail: number of lines cannot be negative: %d\n", n);
     exit(1);
   }
+  if (start_from_line == 0) start_from_line = 1;
 
+  // Read all lines into circular buffer
   while (read(fd, &c, 1) == 1) {
     if (c == '\n' || line_len == MAX_LINE - 1) {
       line[line_len] = '\0';
@@ -85,7 +142,6 @@ int main(int argc, char *argv[]) {
       line[line_len++] = c;
     }
   }
-
   if (line_len > 0) {
     line[line_len] = '\0';
     for (int j = 0; j < MAX_LINE; j++) {
@@ -96,9 +152,17 @@ int main(int argc, char *argv[]) {
     total++;
   }
 
-  int start = total > n ? total - n : 0;
-  for (int i = start; i < total; i++) {
-    printf("%s\n", buf[i % MAX_LINES]);
+  if (start_from_line != -1) {
+    int start = start_from_line - 1;
+    if (start < 0) start = 0;
+    for (int i = start; i < total; i++) {
+      printf("%s\n", buf[i % MAX_LINES]);
+    }
+  } else {
+    int start = total > n ? total - n : 0;
+    for (int i = start; i < total; i++) {
+      printf("%s\n", buf[i % MAX_LINES]);
+    }
   }
 
   if (fd > 0) close(fd);
